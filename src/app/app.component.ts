2584a862265caf061fd80a027858c5c425284390
MaliniUI <malinibe2010@gmail.com>
2020-01-06 17:53:24 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,14 +8,24 @@ import { Component } from '@angular/core';
 export class AppComponent  {
   name = 'Angular';
   menuWidth = 0;
+  isScroll = false;
 
   openNav() {
   // document.getElementById("mySidenav").style.width = "250px";
-  this.menuWidth = 150;
+  this.menuWidth = 300;
+  this.isScroll = false;
 }
 
   closeNav() {
     // document.getElementById("mySidenav").style.width = "0";
     this.menuWidth = 0;
+   // document.getElementsByClassName("content-view-wrap")[0].scrollTo()
   }
+
+ scroll(el: HTMLElement) {
+   this.closeNav();
+   debugger;
+    el.scrollIntoView();
+    this.isScroll = true;
+}
 }
